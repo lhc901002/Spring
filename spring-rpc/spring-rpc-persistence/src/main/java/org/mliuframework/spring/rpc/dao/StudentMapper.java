b4bba11e69e8d142002b6974424d31cbda5e44d6
baidu <baidu@MacBook-Pro.local>
2016-07-13 21:40:04 +0800

    add new project named spring-rpc

---
+++ spring-rpc/spring-rpc-persistence/src/main/java/org/mliuframework/spring/rpc/dao/StudentMapper.java
@@ -0,0 +1,29 @@
+package org.mliuframework.spring.rpc.dao;
+
+import org.mliuframework.spring.rpc.commons.entity.Student;
+import org.mliuframework.spring.rpc.commons.vo.StudentVo;
+
+import java.util.List;
+
+/**
+ * Created by Michael on 7/6/16.
+ */
+public interface StudentMapper {
+
+    StudentVo selectByPrimaryKey(Long id);
+
+    List<StudentVo> selectAll();
+
+    List<StudentVo> selectByProperties(Student student);
+
+    List<StudentVo> selectByName(String name);
+
+    List<StudentVo> selectByIdList(List<Long> idList);
+
+    int insert(Student student);
+
+    int insertSelective(Student student);
+
+    int updateByPrimaryKeySelective(Student student);
+
+}
