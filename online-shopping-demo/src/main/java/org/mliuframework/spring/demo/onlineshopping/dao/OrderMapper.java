31befaf0d393e891a5d7a850c090eb398405daae
techgeeker <haocheng.liu@hotmail.com>
2016-07-25 02:39:37 +0800

    remove online-shopping-demo and spring-orm

--- online-shopping-demo/src/main/java/org/mliuframework/spring/demo/onlineshopping/dao/OrderMapper.java
+++
@@ -1,7 +0,0 @@
-package org.mliuframework.spring.demo.onlineshopping.dao;
-
-/**
- * Created by Michael on 6/29/16.
- */
-public interface OrderMapper {
-}
--- online-shopping-demo/src/main/java/org/mliuframework/spring/demo/onlineshopping/entity/Order.java
+++
@@ -1,7 +0,0 @@
-package org.mliuframework.spring.demo.onlineshopping.entity;
-
-/**
- * Created by Michael on 6/29/16.
- */
-public class Order {
-}
